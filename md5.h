f9730955dec91559587a285b60c0b4253a15f258
lulostupnik <lulostupnik2003@gmail.com>
2024-08-28 11:12:47 -0300

    so far md5 creates forks

---
+++ md5.h
@@ -0,0 +1,15 @@
+#include <sys/select.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>   
+
+#ifndef _MD5_H
+#define _MD5_H
+
+    #define FILES_PER_SLAVE 2
+    #define CANT_SLAVES 5
+
+
+#endif
+
