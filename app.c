1e4cc61955b7bd4ef5e443a92a9b1119fc0105cf
mrossisei <mrossiseifert@itba.edu.ar>
2024-08-28 09:28:24 -0300

    Update comments

---
+++ app.c
@@ -0,0 +1,17 @@
+// master
+
+/*
+master(){
+    while(1){
+        publicar_resultado_en_shm(...); // agreg
+
+
+
+    }
+}
+
+*/
+
+/*
+
+*/
---
+++ view.c
@@ -0,0 +1,9 @@
+
+/*
+vista(){
+    while(sth){
+        leer_resultado_de_shm(...);
+        imprimir_resultado_en_stdout(...);
+    }
+}
+*/
